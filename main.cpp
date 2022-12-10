67e6b2cad5c05f18aafa8892d864f6d60fe58c17
SimoneCastorina <115416896+SimoneCastorina@users.noreply.github.com>
2022-12-10 17:07:04 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
-
+#include "lib.h"
+using namespace std;
 int main(){
+  int n;
+cin>>n;
+float arr [n];
+    for (int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+cout<< numeri(arr,n) <<endl;
   return 0;
 }
