8a662ee6f8f2a22eca94e4fdecd20ca4867cf614
SimoneCastorina <115416896+SimoneCastorina@users.noreply.github.com>
2022-12-10 17:05:57 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
+#include "lib.h"
+float numeri (float arr[],int n)  {
+        for (int i = 0;i<n;++i) {
+            if(arr[0]>arr[i]){
 
+        }else{
+                arr[0]=arr[i];
+            }
+
+        }
+        return arr[0];
+}
